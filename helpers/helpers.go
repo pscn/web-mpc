36259e3f6fc2dd1b51e8ef58c5287791a9e1dc64
pscn <pschnebel@gmail.com>
2019-04-03 16:48:17 +0200

    clean up

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -4,27 +4,22 @@ import (
 	"strconv"
 )
 
-// ToFloat64 the string
-func ToFloat64(str string) float64 {
-	val, err := strconv.ParseFloat(str, 64)
+// ToFloat the string
+func ToFloat(str string) float32 {
+	val, err := strconv.ParseFloat(str, 32)
 	if err != nil {
 		return 0.0
 	}
-	return val
-}
-
-// ToInt64 the string
-func ToInt64(str string) int64 {
-	val, err := strconv.ParseInt(str, 10, 64)
-	if err != nil {
-		return 0
-	}
-	return val
+	return float32(val)
 }
 
 // ToInt the string
 func ToInt(str string) int {
-	return int(ToInt64(str))
+	val, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return int(val)
 }
 
 // ToBool the string
