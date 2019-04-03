a06295067cde5686d9de117854550d5b38c21df2
pscn <pschnebel@gmail.com>
2019-04-03 14:28:43 +0200

    clean up handler code

--- web/handler.go
+++ web/handler.go
@@ -32,6 +32,19 @@ func New(upgrader *websocket.Upgrader, verbosity int) *Handler {
 	}
 }
 
+// getTemplateParameters returns common "constants" shared between go
+// and javascript
+func getTemplateParameters() *map[string]interface{} {
+	p := map[string]interface{}{
+		"error":           mpc.Error,
+		"string":          mpc.Info,
+		"status":          mpc.Status,
+		"currentSong":     mpc.CurrentSong,
+		"currentPlaylist": mpc.Playlist,
+	}
+	return &p
+}
+
 // StaticPacked serves content with contenType
 func (h *Handler) StaticPacked(contentType string, fileName string, box *packr.Box) http.HandlerFunc {
 	tmplStr, err := (*box).FindString(fileName)
@@ -56,16 +69,6 @@ func (h *Handler) StaticFile(contentType string, fileName string) http.HandlerFu
 		}
 		w.Write(dat)
 	}
-}
-func getTemplateParameters() *map[string]interface{} {
-	p := map[string]interface{}{
-		"error":           mpc.Error,
-		"string":          mpc.Info,
-		"status":          mpc.Status,
-		"currentSong":     mpc.CurrentSong,
-		"currentPlaylist": mpc.Playlist,
-	}
-	return &p
 }
 
 // StaticTemplatePacked serves content with contenType
@@ -111,6 +114,7 @@ func (h *Handler) writeMessage(ws *websocket.Conn, msg *mpc.Message) error {
 	}
 	return err
 }
+
 func (h *Handler) readCommand(ws *websocket.Conn) (*mpc.Command, error) {
 	var cmd mpc.Command
 	_, data, err := ws.ReadMessage()
@@ -126,7 +130,8 @@ func (h *Handler) readCommand(ws *websocket.Conn) (*mpc.Command, error) {
 func (h *Handler) Channel(mpdHost string, mpdPass string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		h.logger = log.New(os.Stdout, fmt.Sprintf("web-mpc %s ", r.RemoteAddr), log.LstdFlags|log.Lshortfile)
-		defer func() {
+
+		defer func() { // FIXME: not very nice, but better then crashing eh?
 			if r := recover(); r != nil {
 				h.logger.Println("recovered", r)
 			}
@@ -152,84 +157,72 @@ func (h *Handler) Channel(mpdHost string, mpdPass string) http.HandlerFunc {
 		}
 		defer client.Close()
 
-		// return channel
-		rc := make(chan *mpc.Message, 1)
+		// return channel mpc
+		rc := make(chan *mpc.Message, 10)
 		defer close(rc)
+
+		// channel for websocket
+		wc := make(chan *mpc.Command, 10)
+		defer close(wc)
 
 		go client.EventLoop(rc)
 
-		go func() { // read events from mpc
-
-			for event := range rc {
-				if h.verbosity > 5 {
-					h.logger.Printf("Event: %d\n", event.Type)
-				}
-				switch event.Type {
-				case mpc.Error:
-					h.logger.Println("error:", event.Error())
+		go func() {
+			for {
+				cmd, err := h.readCommand(ws)
+				if err != nil {
+					h.logger.Printf("read error: %v", err)
 					break
-				case mpc.Info:
-					if h.verbosity > 5 {
-						h.logger.Println("string:", event.Info())
-					}
-				case mpc.Status:
-					if h.verbosity > 5 {
-						h.logger.Println("status:", event.Status())
-					}
-				case mpc.CurrentSong:
-					if h.verbosity > 5 {
-						h.logger.Println("current song:", event.CurrentSong())
-					}
-				case mpc.Playlist:
-					if h.verbosity > 5 {
-						h.logger.Println("current playlist:", event.CurrentPlaylist())
-					}
 				}
-				h.writeMessage(ws, event)
+				wc <- cmd
 			}
+			close(wc)
 		}()
 
-		for { // read commands from the webpage
-			cmd, err := h.readCommand(ws) // FIXME: handle err
-			if err != nil {
-				h.logger.Printf("read error: %v", err)
-				break
-			}
-			if h.verbosity > 5 {
-				h.logger.Printf("recv: %v", *cmd)
-			}
-			switch cmd.Command {
-			case mpc.Play:
-				var nr int
-				if cmd.Data != "" {
-					nr = helpers.ToInt(cmd.Data)
-				} else {
-					nr = -1
+		for {
+			select {
+			case msg := <-rc:
+				h.logger.Printf("event: %s\n", *msg)
+				switch msg.Type {
+				case mpc.Error:
+					h.logger.Println("error:", msg.Error())
+					break
 				}
-				err = client.Play(nr)
-			case mpc.Resume:
-				err = client.Resume()
-			case mpc.Pause:
-				err = client.Pause()
-			case mpc.Stop:
-				err = client.Stop()
-			case mpc.Next:
-				err = client.Next()
-			case mpc.Previous:
-				err = client.Previous()
-			case mpc.StatusRequest:
-				rc <- mpc.NewStatus(client.Status())
-			case mpc.Add:
-				err = client.Add(cmd.Data)
-			case mpc.Remove:
-				nr := helpers.ToInt64(cmd.Data)
-				err = client.RemovePlaylistEntry(nr)
-			case mpc.Search:
-				rc <- mpc.NewSearchResult(client.Search(cmd.Data))
-			}
-
-			if err != nil {
-				h.logger.Printf("Command error: %v", err)
+				h.writeMessage(ws, msg)
+			case cmd := <-wc:
+				h.logger.Printf("cmd: %v\n", *cmd)
+				switch cmd.Command {
+				case mpc.Play:
+					var nr int
+					if cmd.Data != "" {
+						nr = helpers.ToInt(cmd.Data)
+					} else {
+						nr = -1
+					}
+					err = client.Play(nr)
+				case mpc.Resume:
+					err = client.Resume()
+				case mpc.Pause:
+					err = client.Pause()
+				case mpc.Stop:
+					err = client.Stop()
+				case mpc.Next:
+					err = client.Next()
+				case mpc.Previous:
+					err = client.Previous()
+				case mpc.StatusRequest:
+					rc <- mpc.NewStatus(client.Status())
+				case mpc.Add:
+					err = client.Add(cmd.Data)
+				case mpc.Remove:
+					nr := helpers.ToInt64(cmd.Data)
+					err = client.RemovePlaylistEntry(nr)
+				case mpc.Search:
+					rc <- mpc.NewSearchResult(client.Search(cmd.Data))
+				}
+				if err != nil {
+					h.logger.Printf("Command error: %v", err)
+				}
 			}
 		}
 	}
