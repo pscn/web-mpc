704966f1f1414dafb9bea978847eaf8fb0b95405
pscn <pschnebel@gmail.com>
2019-04-07 13:58:21 +0200

    themes

--- main.go
+++ main.go
@@ -27,10 +27,14 @@ func main() {
 	if *devel {
 		mux.HandleFunc("/", h.EnsureCookie(h.StaticTemplateFile("text/html", "index.html")))
 		mux.HandleFunc("/script.js", h.StaticFile("text/javascript", "script.js"))
+		mux.HandleFunc("/theme-default.css", h.StaticFile("text/css", "theme-default.css"))
+		mux.HandleFunc("/theme-juri.css", h.StaticFile("text/css", "theme-juri.css"))
 		mux.HandleFunc("/style.css", h.StaticFile("text/css", "style.css"))
 	} else {
 		mux.HandleFunc("/", h.EnsureCookie(h.StaticTemplatePacked("text/html", "index.html", &box)))
 		mux.HandleFunc("/script.js", h.StaticPacked("text/javascript", "script.js", &box))
+		mux.HandleFunc("/theme-default.css", h.StaticPacked("text/css", "theme-default.css", &box))
+		mux.HandleFunc("/theme-juri.css", h.StaticPacked("text/css", "theme-juri.css", &box))
 		mux.HandleFunc("/style.css", h.StaticPacked("text/css", "style.css", &box))
 	}
 	mux.HandleFunc("/ws", h.Channel())
