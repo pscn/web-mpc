5939086dbfd8a180d81a1b9236ecc582c5083e3b
pscn <pschnebel@gmail.com>
2019-04-11 20:40:15 +0200

    be verbose

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 	"github.com/pscn/web-mpc/web"
 )
 
-//go:generate file2go -prefix Index templates/index.html
-//go:generate file2go -prefix CSS templates/style.css
-//go:generate file2go -prefix JavaScript templates/javascript.js
-//go:generate file2go -prefix ThemeDefault templates/theme-default.css
-//go:generate file2go -prefix ThemeJuri templates/theme-juri.css
+//go:generate file2go -verbose -prefix Index templates/index.html
+//go:generate file2go -verbose -prefix CSS templates/style.css
+//go:generate file2go -verbose -prefix JavaScript templates/javascript.js
+//go:generate file2go -verbose -prefix ThemeDefault templates/theme-default.css
+//go:generate file2go -verbose -prefix ThemeJuri templates/theme-juri.css
 
 var addr = flag.String("addr", ":8666", "http service address")
 var mpdHost = flag.String("mpd", "127.0.0.1:6600", "MPD service address")
