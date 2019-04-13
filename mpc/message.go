2888d12962ae6c73b81421b507434c8f84fc6242
pscn <pschnebel@gmail.com>
2019-04-13 23:50:04 +0200

    Fix missing duration for protocol version < 0.20.0

--- mpc/message.go
+++ mpc/message.go
@@ -64,23 +64,23 @@ func VersionMsg(str string) *Message {
 
 // StatusData represents the data we get as MPC status
 type StatusData struct {
-	Duration float32 `json:"duration"`
-	Elapsed  float32 `json:"elapsed"`
-	State    string  `json:"state"`
-	Volume   int     `json:"volume"`
-	Consume  bool    `json:"consume"`
-	Random   bool    `json:"random"`
-	Single   bool    `json:"single"`
-	Repeat   bool    `json:"repeat"`
-	Song     int     `json:"song"`
-	NextSong int     `json:"nextsong"`
+	Duration int    `json:"duration"`
+	Elapsed  int    `json:"elapsed"`
+	State    string `json:"state"`
+	Volume   int    `json:"volume"`
+	Consume  bool   `json:"consume"`
+	Random   bool   `json:"random"`
+	Single   bool   `json:"single"`
+	Repeat   bool   `json:"repeat"`
+	Song     int    `json:"song"`
+	NextSong int    `json:"nextsong"`
 }
 
 // StatusMsg creates a new Event including the status data mapped from mpd.Attrs
 func statusData(attrs *mpd.Attrs) *StatusData {
 	return &StatusData{
-		Duration: conv.ToFloat((*attrs)["duration"]),
-		Elapsed:  conv.ToFloat((*attrs)["elapsed"]),
+		Duration: conv.ToInt((*attrs)["duration"]),
+		Elapsed:  conv.ToInt((*attrs)["elapsed"]),
 		Consume:  conv.ToBool((*attrs)["consume"]),
 		Random:   conv.ToBool((*attrs)["random"]),
 		Single:   conv.ToBool((*attrs)["single"]),
@@ -181,6 +181,9 @@ func UpdateDataMsg(status *mpd.Attrs, song *mpd.Attrs, queue *[]mpd.Attrs,
 	}
 	if event.Status.Song != -1 {
 		event.Queue[event.Status.Song].IsActive = true
+		if event.Status.Duration == -1 { // Protocol Version < 0.20.0
+			event.Status.Duration = event.Queue[event.Status.Song].Duration
+		}
 	}
 	// FIXME: can Song and NextSong be the same?
 	if event.Status.NextSong != -1 {
@@ -188,6 +191,8 @@ func UpdateDataMsg(status *mpd.Attrs, song *mpd.Attrs, queue *[]mpd.Attrs,
 	}
 	// order by song → nextsong → prio
 	sort.Sort(sort.Reverse(queueData(event.Queue)))
+
+	// pagination
 	queueLength := len(event.Queue)
 	fmt.Printf("page: %d\n", queuePage)
 	if queueLength < songsPerPage {
@@ -204,8 +209,8 @@ func UpdateDataMsg(status *mpd.Attrs, song *mpd.Attrs, queue *[]mpd.Attrs,
 		if end > queueLength {
 			end = queueLength
 		}
-		fmt.Printf("page: %d\n", queuePage)
-		fmt.Printf("size: %d; start: %d; end: %d\n", queueLength, start, end)
+		//fmt.Printf("page: %d\n", queuePage)
+		//fmt.Printf("size: %d; start: %d; end: %d\n", queueLength, start, end)
 		event.Queue = event.Queue[start:end]
 	}
 	return NewMessage(Update, event)
