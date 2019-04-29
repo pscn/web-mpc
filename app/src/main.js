df987877875b60c33d6bb6b692bfee60368a6fce
pscn <pschnebel@gmail.com>
2019-04-30 00:06:42 +0200

    Add vue-fragment

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+// provide for "rootless" components aka components with multiple root nodes
+// aka fragments
+import Fragment from "vue-fragment";
+Vue.use(Fragment.Plugin);
 
 Vue.config.productionTip = false;
 
